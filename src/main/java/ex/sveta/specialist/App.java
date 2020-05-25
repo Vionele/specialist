c9d4bd2eb131fc7364195034eefeaac0125172d1
Vionele <lilu-26@ya.ru>
2020-05-25 22:55:17 +0300

    remove comment

--- src/main/java/ex/sveta/specialist/App.java
+++ src/main/java/ex/sveta/specialist/App.java
@@ -8,20 +8,6 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args) {
-        /*Shape p1 = new Point();
-        Shape p2 = new Point(1, 3);
-        Shape c1 = new Circle();
-        Shape c2 = new Circle(10, 20, 3.666);
-
-        List<Shape> shapeList = new ArrayList<Shape>();
-        shapeList.add(p1);
-        shapeList.add(p2);
-        shapeList.add(c1);
-        shapeList.add(c2);
-
-        for (Shape shape : shapeList) {
-            shape.draw();
-        }*/
 
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 //        Shape p1 = ctx.getBean("p1", Shape.class);
